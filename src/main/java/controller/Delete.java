6123ac024a9f4644b74694e885bcfce9145ec6a0
samuelsells123 <samuelsells123@gmail.com>
2021-07-14 16:19:12 -0500

    Added Controller package and update, delete classes

---
+++ src/main/java/controller/Delete.java
@@ -0,0 +1,15 @@
+package controller;
+
+public class Delete {
+    public void deleteUser() {
+
+    }
+
+    public void deleteLocation() {
+
+    }
+
+    public void deleteJourney() {
+
+    }
+}
