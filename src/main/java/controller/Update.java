c7cbb59f7b47a8b4005b32b604bcf3e673d09cf1
samuelsells123 <samuelsells123@gmail.com>
2021-07-14 16:42:11 -0500

    Created All Four Controllers

---
+++ src/main/java/controller/Update.java
@@ -0,0 +1,24 @@
+package controller;
+
+import java.util.List;
+
+import models.Location;
+import models.User;
+
+public class Update {
+	private int journeyId;
+	private User creator;
+	List<Location> locations;
+	
+	public void updateUser() {
+		
+	}
+	
+	public void updateLocation() {
+
+	}
+
+	public void updateJourney() {
+
+	}
+}
