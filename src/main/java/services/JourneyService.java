7a959d55bf72a9765e5a5dea8a6921e0f40d14b6
YunaH <YunaH503@gmail.com>
2021-07-14 17:17:59 -0400

    created services with empty methods, still working on it

---
+++ src/main/java/services/JourneyService.java
@@ -0,0 +1,21 @@
+package src.main.java.services;
+
+public class JourneyService {
+
+    // Implementation
+
+    /* Add Journey */
+    public void addJourney() {
+
+    }
+
+    /* Update Journey */
+    public void updateJourney() {
+
+    }
+
+    /* Remove Journey */
+    public void removeJourney() {
+
+    }
+}
---
+++ src/main/java/services/UserService.java
@@ -0,0 +1,40 @@
+package src.main.java.services;
+
+public class UserService {
+
+    // Implementation
+
+    /* Register User */
+    public void register() {
+
+    }
+
+    /* Login User */
+    public void login() {
+
+    }
+
+    /* Show User by ID */
+    public void showUserById() {
+
+    }
+
+    /* Update User */
+    public void updateUser() {
+        // Update Name
+
+        // Change Password
+
+        // Change Email
+    }
+
+    /* Add Location */
+    public void addLocation() {
+
+    }
+
+    /* Remove Location */
+    public void removeLocation() {
+
+    }
+}
