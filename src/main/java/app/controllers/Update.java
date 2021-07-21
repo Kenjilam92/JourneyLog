5d0fe060c05bc744adb9c21c030c26507574d9b3
samuelsells123 <samuelsells123@gmail.com>
2021-07-20 23:30:04 -0500

    Added JSON Input for Update and Delete Controllers

--- src/main/java/app/controllers/Update.java
+++ src/main/java/app/controllers/Update.java
@@ -22,7 +22,6 @@ public class Update {
 	@PutMapping(path="users/{id}", consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
 	public ResponseEntity<String> updateUser(@PathVariable Integer id, @RequestBody Map<String, Object> userData) {
-
 		System.out.println("Post Update User");
 		System.out.println("First Name: " + userData.get("firstName"));
 		System.out.println("Last Name: " + userData.get("lastName"));
@@ -38,30 +37,57 @@ public class Update {
 			updateUser.setPassword(userData.get("password"));
 
 			if (userService.updateUser(updateUser))
-				return new ResponseEntity<>("{ \"connected\" : true," +
-													"\"status\" : \"success\" }",
-													HttpStatus.OK);
+				return new ResponseEntity<>("{ \"connected\" : true, \"status\" : \"success\" }", HttpStatus.OK);
 		}*/
 
-		return new ResponseEntity<>("{ \"connected\" : true," +
-											"\"status\" : \"failure\" }",
-											HttpStatus.OK);
+		return new ResponseEntity<>("{ \"connected\" : false, \"status\" : \"failure\" }", HttpStatus.OK);
 	}
 
-	@PutMapping(path="location/{id}", consumes=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity updateLocation(@RequestBody Location l, @PathVariable Integer id) {
+	@PutMapping(path="location/{id}", consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> updateLocation(@PathVariable Integer id, @RequestBody Map<String, Object> locationData) {
 		System.out.println("Post Update Location");
-		return null;
-		//userService.updateLocation(l);
-		//return ResponseEntity.noContent().build();
+		System.out.println("Street Number: " + locationData.get("streetNumber"));
+		System.out.println("Street Name: " + locationData.get("streetName"));
+		System.out.println("City: " + locationData.get("city"));
+		System.out.println("State: " + locationData.get("state"));
+		System.out.println("Zipcode: " + locationData.get("zipcode"));
+
+
+		/*Location updateLocation = locationService.getLocationById(id);
+
+		if(updateLocation != null) {
+			updateLocation.setStreetNumber(locationData.get("streetNumber"));
+			updateLocation.setStreetName(locationData.get("streetName"));
+			updateLocation.setCity(locationData.get("city"));
+			updateLocation.setState(locationData.get("state"));
+			updateLocation.setZipcode(locationData.get("zipcode"));
+
+			if (locationService.updateLocation(updateLocation))
+				return new ResponseEntity<>("{ \"connected\" : true, \"status\" : \"success\" }", HttpStatus.OK);
+		}*/
+
+		return new ResponseEntity<>("{ \"connected\" : false, \"status\" : \"failure\" }", HttpStatus.OK);
 	}
 
-	@PutMapping(path="journey/{id}", consumes=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity updateJourney(@RequestBody Journey j, @PathVariable Integer id) {
+	@PutMapping(path="journey/{id}", consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity updateJourney(@PathVariable Integer id, @RequestBody Map<String, Object> journeyData) {
 		System.out.println("Post Update Journey");
-		return null;
-		//userService.updateJourney(j);
-		//return ResponseEntity.noContent().build();
+		System.out.println("Creator ID: " + journeyData.get("creatorId"));
+		System.out.println("Time: " + journeyData.get("time"));
+		System.out.println("Length: " + journeyData.get("length"));
+
+		/*Journey updateJourney = journeyService.getJourneyById(id);
+
+		if(updateJourney != null) {
+			updateJourney.setCreator(userService.getUserById(journeyData.get("creatorId")));
+			updateJourney.setTime(journeyData.get("time"));
+			updateJourney.setLength(journeyData.get("length"));
+
+			if (journeyService.updateJourney(updateJourney))
+				return new ResponseEntity<>("{ \"connected\" : true, \"status\" : \"success\" }", HttpStatus.OK);
+		}*/
+
+		return new ResponseEntity<>("{ \"connected\" : false, \"status\" : \"failure\" }", HttpStatus.OK);
 	}
 
 }
