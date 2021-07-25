94ec2e093bcb0234acb2e151909d8d7cac6154d0
YunaH <YunaH503@gmail.com>
2021-07-25 14:56:48 -0400

    imported necessary table modules

--- client/JourneyLog-FrontEnd/src/app/app.module.ts
+++ client/JourneyLog-FrontEnd/src/app/app.module.ts
@@ -1,25 +1,41 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppComponent } from './app.component';
 import { TestServicesComponent } from './components/test-services/test-services.component';
 import { JourneyFormComponent } from './components/journey-form/journey-form.component';
 import { LocationTableComponent } from './components/location-table/location-table.component';
+import { JourneyTableComponent } from './components/journey-table/journey-table.component';
+import { TableTabsComponent } from './components/table-tabs/table-tabs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestServicesComponent,
     JourneyFormComponent,
-    LocationTableComponent
+    LocationTableComponent,
+    JourneyTableComponent,
+    TableTabsComponent
+
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
-
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatTabsModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
