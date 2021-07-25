e158728fd30f4b23709aefc9471b1805e7da5b76
YunaH <YunaH503@gmail.com>
2021-07-25 14:55:45 -0400

    Added tabs for address and journey table, modified app component to show table tabs component

---
+++ client/JourneyLog-FrontEnd/src/app/components/journey-table/journey-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-journey-table',
+  templateUrl: './journey-table.component.html',
+  styleUrls: ['./journey-table.component.css']
+})
+export class JourneyTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
