da1d56165294c32259b32ebc98fa5d8992402d3d
albraet <38644447+albraet@users.noreply.github.com>
2021-07-27 05:03:17 -0400

    update
    I added two components

---
+++ client/JourneyLog-FrontEnd/src/app/app.module.ts
@@ -0,0 +1,55 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+
+
+import { AppComponent } from './app.component';
+import { TestServicesComponent } from './components/test-services/test-services.component';
+import { JourneyFormComponent } from './components/journey-form/journey-form.component';
+import { LocationTableComponent } from './components/location-table/location-table.component';
+import { JourneyTableComponent } from './components/journey-table/journey-table.component';
+import { TableTabsComponent } from './components/table-tabs/table-tabs.component';
+import { MetterToMilesPipe } from './pipes/metter-to-miles.pipe';
+import { SecondsToTimeStringPipe } from './pipes/seconds-to-time-string.pipe';
+import { LoginComponent } from './components/login/login/login.component';
+import { RegisterComponent } from './components/register/register/register.component';
+;
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TestServicesComponent,
+    JourneyFormComponent,
+    LocationTableComponent,
+    JourneyTableComponent,
+    TableTabsComponent,
+    MetterToMilesPipe,
+    SecondsToTimeStringPipe,
+    LoginComponent,
+    RegisterComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatTabsModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatCardModule,
+    MatExpansionModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
